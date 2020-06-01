f5659ef7080194345795c2043f1897a6071cfb3c
Mark McClean <mcclean_mark_02@hotmail.com>
2020-06-01 17:53:43 +0000

    environment variables added and referred to in app.py file. Gitignore file also added to store environment variables.

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+
+
+app.config["MONGODB_NAME"] = os.environ.get("MONGODB_NAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+COLLECTION_NAME = "lego_catalogue"
+
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
