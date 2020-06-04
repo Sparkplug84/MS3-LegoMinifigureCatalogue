448871cd3b6c486e73c806c512184953ec566377
Mark McClean <mcclean_mark_02@hotmail.com>
2020-06-04 20:21:44 +0000

    add minifig form now submits and info is stored in Mongo along with uploaded photo. Record is then retrieved and visible on the minifigs.html page

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_minifigures')
 def get_minifigures():
-    return render_template("minifigures.html", minifigures=mongo.db.minifigures.find())
+    return render_template("minifigs.html", minifigures=mongo.db.minifigures.find())
 
 
 @app.route('/add_minifig')
@@ -29,6 +29,26 @@ def add_minifig():
     age=mongo.db.age.find(),
     parts=mongo.db.parts.find(),
     rarity=mongo.db.rarity.find())
+
+
+@app.route('/insert_minifig', methods=['POST'])
+def insert_minifig():
+    if 'photo' in request.files:
+        photo = request.files['photo']
+        mongo.save_file(photo.filename, photo)
+        mongo.db.minifigures.insert({'photo': photo.filename, 
+                            'minifigure_name': request.form.get('minifigure_name'),
+                            'theme_name': request.form.get('theme_name'),
+                            'age_range': request.form.get('age_range'),
+                            'feature': request.form.get('feature'),
+                            'number_of_parts': request.form.get('number_of_parts'),
+                            'rarity_name': request.form.get('rarity_name')})
+    return redirect(url_for('get_minifigures'))
+
+
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
 
 
 if __name__ == '__main__':
