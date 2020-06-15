927d99878bb4b0603f0f670bd2e35ecbcb6a9c4e
Mark McClean <mcclean_mark_02@hotmail.com>
2020-06-15 20:35:47 +0000

    created a function to add and remove filter options from minifigs.html page

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,25 @@
 const searchName = document.getElementById('searchName');
 const searchBox = document.getElementById('search-box');
+const searchOptions = document.getElementById('searchOptions');
 
-searchName.onclick = function () { addSearch() };
-function addSearch() {
-    searchBox.classList.remove("d-none");
+
+
+if(searchName) {
+    function addSearch() {
+        if (searchOptions.value=="1") {
+            searchBox.classList.remove("d-none");
+        } else if (searchOptions.value=="2") {
+            searchBox.classList.add("d-none");
+        } else {
+            searchBox.classList.remove("d-none");
+        }
+    }
 }
+
+// if(searchName) {
+//     searchName.onchange = function () { addSearch() };
+//     function addSearch() {
+//         searchBox.classList.remove("d-none");
+//         // print ("Hello!!!")
+//     }
+// }
