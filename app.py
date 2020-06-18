2510ab60ca369504bbfa2c7fd5f7133a78f58b48
Mark McClean <mcclean_mark_02@hotmail.com>
2020-06-18 21:08:25 +0000

    changed filter dropdown select to a dropdown button with list items so anchor tags could be added. Filter function now working to a certain degree.

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ COLLECTION_NAME = "lego_catalogue"
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/index_page')
 def index_page():
@@ -25,16 +26,17 @@ def index_page():
 @app.route('/get_minifigures')
 def get_minifigures():
     minifigures = mongo.db.minifigures.find({"minifig_deleted": False})
-    return render_template("minifigs.html", minifigures=minifigures)
+    return render_template("minifigs.html", minifigures=minifigures, themes=mongo.db.themes.find(),
+                           age=mongo.db.age.find())
 
 
 @app.route('/add_minifig')
 def add_minifig():
     return render_template('addminifig.html',
-    themes=mongo.db.themes.find(),
-    age=mongo.db.age.find(),
-    parts=mongo.db.parts.find(),
-    rarity=mongo.db.rarity.find())
+                           themes=mongo.db.themes.find(),
+                           age=mongo.db.age.find(),
+                           parts=mongo.db.parts.find(),
+                           rarity=mongo.db.rarity.find())
 
 
 @app.route('/insert_minifig', methods=['POST'])
@@ -42,14 +44,14 @@ def insert_minifig():
     if 'photo' in request.files:
         photo = request.files['photo']
         mongo.save_file(photo.filename, photo)
-        mongo.db.minifigures.insert_one({'photo': photo.filename, 
-                            'minifigure_name': request.form.get('minifigure_name'),
-                            'theme_name': request.form.get('theme_name'),
-                            'age_range': request.form.get('age_range'),
-                            'feature': request.form.get('feature'),
-                            'number_of_parts': request.form.get('number_of_parts'),
-                            'rarity_name': request.form.get('rarity_name'),
-                            'minifig_deleted': False})
+        mongo.db.minifigures.insert_one({'photo': photo.filename,
+                                         'minifigure_name': request.form.get('minifigure_name'),
+                                         'theme_name': request.form.get('theme_name'),
+                                         'age_range': request.form.get('age_range'),
+                                         'feature': request.form.get('feature'),
+                                         'number_of_parts': request.form.get('number_of_parts'),
+                                         'rarity_name': request.form.get('rarity_name'),
+                                         'minifig_deleted': False})
     return redirect(url_for('get_minifigures'))
 
 
@@ -60,7 +62,8 @@ def file(filename):
 
 @app.route('/edit_minifig/<minifigure_id>')
 def edit_minifig(minifigure_id):
-    the_minifig = mongo.db.minifigures.find_one({"_id": ObjectId(minifigure_id)})
+    the_minifig = mongo.db.minifigures.find_one(
+        {"_id": ObjectId(minifigure_id)})
     all_themes = mongo.db.themes.find()
     all_age = mongo.db.age.find()
     all_parts = mongo.db.parts.find()
@@ -71,23 +74,23 @@ def edit_minifig(minifigure_id):
 @app.route('/update_minifig/<minifigure_id>', methods=['POST'])
 def update_minifig(minifigure_id):
     mongo.db.minifigures.update_one({'_id': ObjectId(minifigure_id)},
-    { "$set": {
-        'minifigure_name': request.form.get('minifigure_name'),
-        'theme_name': request.form.get('theme_name'),
-        'age_range': request.form.get('age_range'),
-        'feature': request.form.get('feature'),
-        'number_of_parts': request.form.get('number_of_parts'),
-        'rarity_name': request.form.get('rarity_name')
-    }})
+                                    {"$set": {
+                                        'minifigure_name': request.form.get('minifigure_name'),
+                                        'theme_name': request.form.get('theme_name'),
+                                        'age_range': request.form.get('age_range'),
+                                        'feature': request.form.get('feature'),
+                                        'number_of_parts': request.form.get('number_of_parts'),
+                                        'rarity_name': request.form.get('rarity_name')
+                                    }})
     return redirect(url_for('get_minifigures'))
 
 
 @app.route('/delete_minifig/<minifigure_id>')
 def delete_minifig(minifigure_id):
     mongo.db.minifigures.update_one({'_id': ObjectId(minifigure_id)},
-    { "$set": {
-        'minifig_deleted': True }
-    })
+                                    {"$set": {
+                                        'minifig_deleted': True}
+                                     })
     return redirect(url_for('get_minifigures'))
 
 
@@ -104,9 +107,9 @@ def edit_theme(theme_id):
 @app.route('/update_theme/<theme_id>', methods=['POST'])
 def update_theme(theme_id):
     mongo.db.themes.update_one({'_id': ObjectId(theme_id)},
-    { "$set": {
-        'theme_name': request.form.get('theme_name') }
-    })
+                               {"$set": {
+                                   'theme_name': request.form.get('theme_name')}
+                                })
     return redirect(url_for('get_themes'))
 
 
@@ -125,6 +128,14 @@ def insert_theme():
 @app.route('/add_theme')
 def add_theme():
     return render_template('addtheme.html')
+
+
+@app.route('/get_minifigure_theme/<theme_name>')
+def get_minifigure_theme(theme_name):
+    print(theme_name)
+    minifigure_theme = mongo.db.minifigures.find(
+        {'theme_name': theme_name})
+    return render_template("minifigs.html", minifigure_theme=minifigure_theme)
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
 const searchName = document.getElementById('searchName');
 const searchBox = document.getElementById('search-box');
 const searchOptions = document.getElementById('searchOptions');
+const themeBox = document.getElementById('theme-box');
+const ageBox = document.getElementById('age-box');
 
 
 
@@ -8,18 +10,16 @@ if(searchName) {
     function addSearch() {
         if (searchOptions.value=="1") {
             searchBox.classList.remove("d-none");
+            themeBox.classList.add("d-none");
+            ageBox.classList.add("d-none");
         } else if (searchOptions.value=="2") {
             searchBox.classList.add("d-none");
+            themeBox.classList.remove("d-none");
+            ageBox.classList.add("d-none");
         } else {
-            searchBox.classList.remove("d-none");
+            searchBox.classList.add("d-none");
+            themeBox.classList.add("d-none");
+            ageBox.classList.remove("d-none");
         }
     }
 }
-
-// if(searchName) {
-//     searchName.onchange = function () { addSearch() };
-//     function addSearch() {
-//         searchBox.classList.remove("d-none");
-//         // print ("Hello!!!")
-//     }
-// }
